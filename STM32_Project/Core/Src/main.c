9bf01870fccc0081a5300ab06c78e9b635ccdfeb
Jeype07 <131168989+Jeype07@users.noreply.github.com>
2024-11-08 13:54:48 +0100

    Modif projet stm32 temp&press measure

--- STM32_Project/Core/Src/main.c
+++ STM32_Project/Core/Src/main.c
@@ -96,6 +96,42 @@ void read_calibration_data();
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
+// Returns temperature in DegC, double precision. Output value of “51.23” equals 51.23 DegC.
+// t_fine carries fine temperature as global value
+BMP280_S32_t t_fine;
+double bmp280_compensate_T_double(BMP280_S32_t adc_T)
+{
+	double var1, var2, T;
+	var1 = (((double)adc_T)/16384.0 - ((double)dig_T1)/1024.0) * ((double)dig_T2);
+	var2 = ((((double)adc_T)/131072.0 - ((double)dig_T1)/8192.0) *
+	(((double)adc_T)/131072.0 - ((double) dig_T1)/8192.0)) * ((double)dig_T3);
+	t_fine = (BMP280_S32_t)(var1 + var2);
+	T = (var1 + var2) / 5120.0;
+	return T;
+}
+
+// Returns pressure in Pa as double. Output value of “96386.2” equals 96386.2 Pa = 963.862 hPa
+double bmp280_compensate_P_double(BMP280_S32_t adc_P)
+{
+	double var1, var2, p;
+	var1 = ((double)t_fine/2.0) - 64000.0;
+	var2 = var1 * var1 * ((double)dig_P6) / 32768.0;
+	var2 = var2 + var1 * ((double)dig_P5) * 2.0;
+	var2 = (var2/4.0)+(((double)dig_P4) * 65536.0);
+	var1 = (((double)dig_P3) * var1 * var1 / 524288.0 + ((double)dig_P2) * var1) / 524288.0;
+	var1 = (1.0 + var1 / 32768.0)*((double)dig_P1);
+	if (var1 == 0.0)
+	{
+		return 0; // avoid exception caused by division by zero
+	}
+	p = 1048576.0 - (double)adc_P;
+	p = (p - (var2 / 4096.0)) * 6250.0 / var1;
+	var1 = ((double)dig_P9) * p * p / 2147483648.0;
+	var2 = p * ((double)dig_P8) / 32768.0;
+	p = p + (var1 + var2 + ((double)dig_P7)) / 16.0;
+	return p;
+}
+
 /* USER CODE END 0 */
 
 /**
@@ -148,7 +184,7 @@ int main(void)
 	uint8_t calib_reg[1];
 	uint8_t calib_data[BMP_CALIB_DATA_LENGTH];
 	Struct_CalibDataNames *calib_names;
-	int32_t *temp; int32_t *press; // raw values
+	int32_t *temp, *press; // raw values
 
 	//question réponse capteur avec I2C pour ID capteur
 	id_buf[0]= BMP_ID_REG;
